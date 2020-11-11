2a001a184c5332ebff2d8c29c976dae7386e4a66
hunman89 <hunman89@gmail.com>
2020-11-11 21:30:44 +0900

    Add github login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from django.views import View
 from django.views.generic import FormView
 from django.urls import reverse_lazy
@@ -59,3 +61,78 @@ def complete_verification(request, key):
         # to do : add error message
         pass
     return redirect(reverse("core:home"))
+
+
+def github_login(request):  # 로그인 1단계
+    client_id = os.environ.get("GH_ID")
+    redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
+    return redirect(
+        f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user"
+    )
+
+
+class GithubException(Exception):
+    pass
+
+
+def github_callback(request):  # 2단계
+    try:
+        client_id = os.environ.get("GH_ID")
+        client_secret = os.environ.get("GH_SECRET")
+        code = request.GET.get("code", None)
+        if code is not None:
+            token_request = requests.post(
+                f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
+                headers={"Accept": "application/json"},
+            )
+            token_json = token_request.json()
+            error = token_json.get("error", None)  # json 에러 체크
+            if error is not None:
+                raise GithubException()
+            else:
+                access_token = token_json.get("access_token")  # json에서 가져온 토큰
+                profile_request = requests.get(
+                    "https://api.github.com/user",
+                    headers={
+                        "Authorization": f"token {access_token}",
+                        "Accept": "application/json",
+                    },
+                )
+                profile_json = profile_request.json()  # request이용 profile을 가져온다, 3단계
+                username = profile_json.get("login", None)
+                if username is not None:  # profile 체크
+                    name = profile_json.get("name")
+                    email = profile_json.get("email")
+                    bio = profile_json.get("bio")
+                    try:
+                        user = models.User.objects.get(email=email)
+                        if (
+                            user.login_method != models.User.LOGIN_GITHUB
+                        ):  # 다른 방식의 로그인인 경우 = 다시 로그인하게
+                            raise GithubException
+                    except models.User.DoesNotExist:  # 존재하지 않는 user면 새로운거 만든다
+                        user = models.User.objects.create(
+                            email=email,
+                            first_name=name,
+                            username=email,
+                            bio=bio,
+                            login_method=models.User.LOGIN_GITHUB,
+                        )
+                        user.set_unusable_password()  # 비밀번호 사용 못하게 = 깃허브 로그인이니까
+                        user.save()
+                    login(request, user)  # 로그인
+                    return redirect(reverse("core:home"))
+                    if user is not None:
+                        return redirect(reverse("users:login"))
+                    else:
+                        user = models.User.objects.create(
+                            username=email, first_name=name, bio=bio, email=email
+                        )
+                        login(request, user)
+                        return redirect(reverse("core:home"))
+                else:
+                    raise GithubException()
+        else:
+            raise GithubException()
+    except GithubException:  # 에러를 하나로 합침
+        return redirect(reverse("users:login"))
